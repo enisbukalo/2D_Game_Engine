85163e01d2361e1418c8f6e349e781ff906fa881
Enis Bukalo <bukaloenis@gmail.com>
2025-01-14 22:30:27 -0500

    Update Vec2.h

--- include/Vec2.h
+++ include/Vec2.h
@@ -7,45 +7,45 @@
 class Vec2
 {
 public:
-    float x = 0.0;
+    float x = 0.0f;
     float y = 0.0f;
 
-    Vec2() {};
-    Vec2(float x, float y) : x(x), y(y) {};
+    Vec2() = default;
+    Vec2(float x, float y) : x(x), y(y) {}
 
     // Calculate the length of the vector
     float length()
     {
         return sqrt(x * x + y * y);
-    };
+    }
 
     // Normalize the vector
-    Vec2 &normalize()
+    Vec2& normalize()
     {
         float l = length();
         x /= l;
         y /= l;
         return *this;
-    };
+    }
 
     // Add two vectors
-    Vec2 &add(const Vec2 &other)
+    Vec2& add(const Vec2& other)
     {
         x += other.x;
         y += other.y;
         return *this;
-    };
+    }
 
     // Scale the vector
-    Vec2 &scale(float multiplier)
+    Vec2& scale(float multiplier)
     {
         x *= multiplier;
         y *= multiplier;
         return *this;
-    };
+    }
 
     // Rotate the vector
-    Vec2 &rotate(float angle)
+    Vec2& rotate(float angle)
     {
         float c    = cos(angle);
         float s    = sin(angle);
@@ -54,17 +54,17 @@ public:
         x          = newX;
         y          = newY;
         return *this;
-    };
+    }
 
     // Calculate the distance between two vectors
-    float distance(const Vec2 &other)
+    float distance(const Vec2& other)
     {
         return sqrtf((other.x - x) * (other.x - x) + (other.y - y) * (other.y - y));
-    };
+    }
 
 #pragma region Operators
     // Add two vectors
-    Vec2 &operator+=(const Vec2 &other)
+    Vec2& operator+=(const Vec2& other)
     {
         x += other.x;
         y += other.y;
@@ -72,7 +72,7 @@ public:
     }
 
     // Subtract two vectors
-    Vec2 &operator-=(const Vec2 &other)
+    Vec2& operator-=(const Vec2& other)
     {
         x -= other.x;
         y -= other.y;
@@ -80,25 +80,25 @@ public:
     }
 
     // Add two vectors
-    Vec2 operator+(const Vec2 &other) const
+    Vec2 operator+(const Vec2& other) const
     {
         return Vec2(x + other.x, y + other.y);
     }
 
     // Subtract two vectors
-    Vec2 operator-(const Vec2 &other) const
+    Vec2 operator-(const Vec2& other) const
     {
         return Vec2(x - other.x, y - other.y);
     }
 
     // Multiply the vector by a scalar
-    Vec2 operator*(const float &multiplier) const
+    Vec2 operator*(const float& multiplier) const
     {
         return Vec2(x * multiplier, y * multiplier);
     }
 
     // Divide the vector by a scalar
-    Vec2 operator/(const float &divisor) const
+    Vec2 operator/(const float& divisor) const
     {
         return Vec2(x / divisor, y / divisor);
     }
