2a82acd393da09086498d36957f4795b4a5fcb80
Enis Bukalo <bukaloenis@gmail.com>
2025-01-15 13:01:24 -0500

    Library Update
    - Converted to be able to build for a library.
    - Builds tests.

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,21 +1,13 @@
 #include "GameEngine.h"
 
-GameEngine::GameEngine(sf::RenderWindow *window, b2Vec2 gravity, uint8_t subStepCount, float timeStep)
+GameEngine::GameEngine(sf::RenderWindow *window, sf::Vector2f gravity, uint8_t subStepCount, float timeStep)
     : m_window(window), m_gravity(gravity), m_subStepCount(subStepCount), m_timeStep(timeStep)
 {
     m_window->setFramerateLimit(144);
-
-    m_worldDef         = b2DefaultWorldDef();
-    m_worldDef.gravity = m_gravity;
-    m_worldId          = b2CreateWorld(&m_worldDef);
-
     m_gameRunning = true;
 }
 
-GameEngine::~GameEngine()
-{
-    b2DestroyWorld(m_worldId);
-}
+GameEngine::~GameEngine() {}
 
 void GameEngine::readInputs()
 {
@@ -31,16 +23,12 @@ void GameEngine::readInputs()
 
 void GameEngine::update()
 {
-    b2World_Step(m_worldId, m_timeStep, m_subStepCount);
+    // Update game logic here
 }
 
 void GameEngine::render()
 {
     m_window->clear();
-    //   for (sf::Shape *shape : shapes) {
-    //     Game::window.draw(*shape);
-    //   }
-
-    //   Game::window.draw(lines);
+    // Render game objects here
     m_window->display();
 }
