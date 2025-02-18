9a94f9f96b9418f584785c05635d710b794333a0
Enis Bukalo <bukaloenis@gmail.com>
2025-02-18 14:31:27 -0500

    Added Tests
    - Added test for CircleCollider.
    - Added AABB Test.
    - Added Vec2 Test.
    - Updated logic for AABB.

--- src/AABB.cpp
+++ src/AABB.cpp
@@ -1,17 +1,21 @@
+#include <cmath>  // for std::abs
 #include "physics/Quadtree.h"
 
-AABB::AABB(const Vec2& pos, const Vec2& size) : position(pos), halfSize(size) {}
+AABB::AABB(const Vec2& pos, const Vec2& size) : position(pos), halfSize(Vec2(std::abs(size.x), std::abs(size.y))) {}
 
 bool AABB::contains(const Vec2& point) const
 {
-    return (point.x >= position.x - halfSize.x && point.x <= position.x + halfSize.x
-            && point.y >= position.y - halfSize.y && point.y <= position.y + halfSize.y);
+    return (point.x >= position.x - std::abs(halfSize.x) && point.x <= position.x + std::abs(halfSize.x)
+            && point.y >= position.y - std::abs(halfSize.y) && point.y <= position.y + std::abs(halfSize.y));
 }
 
 bool AABB::intersects(const AABB& other) const
 {
-    return !(position.x + halfSize.x < other.position.x - other.halfSize.x
-             || position.x - halfSize.x > other.position.x + other.halfSize.x
-             || position.y + halfSize.y < other.position.y - other.halfSize.y
-             || position.y - halfSize.y > other.position.y + other.halfSize.y);
+    float thisHalfX  = std::abs(halfSize.x);
+    float thisHalfY  = std::abs(halfSize.y);
+    float otherHalfX = std::abs(other.halfSize.x);
+    float otherHalfY = std::abs(other.halfSize.y);
+
+    return !(position.x + thisHalfX < other.position.x - otherHalfX || position.x - thisHalfX > other.position.x + otherHalfX
+             || position.y + thisHalfY < other.position.y - otherHalfY || position.y - thisHalfY > other.position.y + otherHalfY);
 }
